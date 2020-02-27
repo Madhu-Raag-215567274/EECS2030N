313885c8e610b474ef8a850caebda6f6b2e1a45e
Madhu-Raag-215567274 <raag@my.yorku.ca>
2020-02-27 01:49:31 -0500

    lab4

--- EECS2030_LAB4W20_stu/src/myUtil/MyArrayList.java
+++ EECS2030_LAB4W20_stu/src/myUtil/MyArrayList.java
@@ -63,6 +63,16 @@ public class MyArrayList<E> implements MyList<E> {
 
 	@Override
 	public void add(E o) {
+		if(this.elementData[this.elementData.length-1]!=null) {
+			E [] temp = (E[])new Object [this.elementData.length];			
+			for(int i=0;i<this.elementData.length;i++) {
+				temp[i] =this.elementData[i];
+			}
+			this.elementData = (E[])new Object [temp.length+DEFAULT_CAPACITY];
+			for(int i=0;i<temp.length;i++) {
+				this.elementData[i]=temp[i];
+			}
+		}
 			
 		int counter=0;
 		boolean p=true;
@@ -78,6 +88,11 @@ public class MyArrayList<E> implements MyList<E> {
 			this.size++;
 			
 		}
+		
+		System.out.println("---------");
+		for(int i=0;i<this.elementData.length;i++) {
+			System.out.println(this.elementData[i]);
+		}
 			
 			
 	
@@ -86,31 +101,141 @@ public class MyArrayList<E> implements MyList<E> {
 	@Override
 	public void remove(E o) {
 		// TODO Auto-generated method stub
+		size=0;
+		boolean q=true;
+		for(int i =0;i<this.elementData.length&& q;i++) {
+			if(elementData[i]==null) {
+				q=false;
+			}else {
+				this.size++;
+			}
+		}
+		boolean p=true;
+		int counter=-1;
+		for(int i =0; i<this.elementData.length && p;i++) {
+			if(this.elementData[i]==o) {
+				counter =i;
+				p=false;
+			}
+		}
+		
+		int index=counter;
+		if(counter ==-1) {
+			
+		}
+		else {
+			if(index==this.elementData.length-1) {
+				this.elementData[this.elementData.length-1]=null;
+			}
+			else {
+			this.elementData[counter]=null;
+			for(int i= counter;i<this.elementData.length-1;i++) {
+				this.elementData[i]=this.elementData[i+1];
+			}
+			this.elementData[this.elementData.length-1]=null;
+			
+		}
+	  }
+		size--;
 		
 	}
 
 	@Override
 	public E get(int index) {
 		// TODO Auto-generated method stub
+		
 		return this.elementData[index];
 	}
 
 	@Override
 	public E set(int index, E element) {
+		if(index>this.elementData.length-1 || this.elementData[index]==null ) {
+			throw new IndexOutOfBoundsException();
+		}
+		E p = this.elementData[index];
+		this.elementData[index]=element;
 		// TODO Auto-generated method stub
-		return null;
+		
+		return p ;
 	}
 
 	@Override
 	public void add(int index, E element) {
 		// TODO Auto-generated method stub
+		size=0;
+		boolean q=true;
+		for(int i =0;i<this.elementData.length&& q;i++) {
+			if(elementData[i]==null) {
+				q=false;
+			}else {
+				this.size++;
+			}
+		}
+		
+		
+	E p= (elementData[index]);
+		int counter=index;
+		
+		if(index==this.elementData.length-1) {
+		
+			this.elementData[counter]=element;
+			this.elementData[this.elementData.length] =p;
+		}
+		else {
+		
+			
+			
+		for(int i=this.elementData.length-1 ;i>counter;i--) {			
+			this.elementData[i]=this.elementData[i-1];	
+			 
+			
+		}
+
+		this.elementData[index]=element;
+		}
+	
+			
+		
+	  
+		size++;
+		
 		
 	}
 
 	@Override
 	public E remove(int index) {
+		if(elementData[index]==null) {
+			throw new IndexOutOfBoundsException();
+		}
+		size=0;
+		boolean q=true;
+		for(int i =0;i<this.elementData.length&& q;i++) {
+			if(elementData[i]==null) {
+				q=false;
+			}else {
+				this.size++;
+			}
+		}
+		E p= (elementData[index]);
+		int counter=index;
+	//	index=counter;
+		
+		if(index==this.elementData.length-1) {
+				this.elementData[this.elementData.length-1]=null;
+			}
+			else {
+			this.elementData[counter]=null;
+			for(int i= counter;i<this.elementData.length-1;i++) {
+				this.elementData[i]=this.elementData[i+1];
+			}
+			this.elementData[this.elementData.length-1]=null;
+			
+		}
+	  
+		size--;
+		
 		// TODO Auto-generated method stub
-		return null;
+		return p;
 	}
 
 	@Override
